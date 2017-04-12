312f31f3678a47deebce02d0226bc7800c7a5474
kubocker <kubocker@gmail.com>
2017-04-12 15:58:20 +0900

    setup.py add

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(name='cute.py',
+      version='0.0.1',
+      description='common ui and commands for pyqt5',
+      author='kubocker',
+      author_email='kubocker@gmail.com',)
