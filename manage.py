a968e3b9c5b0ca6c4db2f9686955230c1853e70b
kubocker <kubocker@gmail.com>
2017-05-04 12:52:00 +0900

    startapp command 実装

--- manage.py
+++ manage.py
@@ -1,4 +1,7 @@
+import subprocess
 import fire
+
+files = ['__init__.py', 'forms.py', 'views.py', 'models.py']
 
 
 class Command(object):
@@ -10,7 +13,15 @@ class Command(object):
         pass
 
     def startapp(self, name):
-        return "make app: " + name
+        cmd = "cd ../ && mkdir {0}".format(name)
+        subprocess.call(cmd, shell=True)
+
+        print("creating...")
+        for k in range(len(files)):
+            cmd = "cd ../{0} && touch {1}".format(name, files[k])
+            print(name + "/" + files[k])
+            subprocess.call(cmd, shell=True)
+
 
 if __name__ == '__main__':
     fire.Fire(Command)
