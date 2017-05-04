599006f9060ca343eac0fdb16f37a8db6fe9fcb6
kubocker <kubocker@gmail.com>
2017-05-04 11:55:47 +0900

    想定するコマンドを追加

--- manage.py
+++ manage.py
@@ -6,5 +6,11 @@ class Command(object):
     def hello(self, username):
         return "Hello, " + username + " !!"
 
+    def startproject(self, name):
+        pass
+
+    def startapp(self, name):
+        return "make app: " + name
+
 if __name__ == '__main__':
     fire.Fire(Command)
