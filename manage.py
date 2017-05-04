98b4bf4ae2b5c05a501030ed20be93d69baca276
kubocker <kubocker@gmail.com>
2017-05-04 16:44:44 +0900

    ファイル生成コマンド 実装

--- manage.py
+++ manage.py
@@ -11,24 +11,35 @@ class Command(object):
         """ テストテスト """
         return "Hello, " + username + " !!"
 
+    def __create_files(self, name, files=[]):
+        cmd = "cd ../ && mkdir {0}".format(name)
+        subprocess.call(cmd, shell=True)
+
+        print("creating...")
+        for k in range(len(files)):
+            cmd = "cd ../{0} && touch {1}".format(name, files[k])
+            print(name + "/" + files[k])
+            subprocess.call(cmd, shell=True)
+
     def startproject(self, name):
         """ プロジェクトディレクトリを作成する
         @param name
         """
-        pass
+        print("------ project dir --------")
+        self.__create_files(name, files=project_files)
+
+        copy_files = ['__init_.py', 'settings.py']
+        for k in range(len(copy_files)):
+            if copy_files[k] in project_files:
+                cmd = "cp base/{0} ../{1}/{2}".format(copy_files[k], name, project_files[k])
+                subprocess.call(cmd, shell=True)
 
     def startapp(self, name):
         """ アプリディレクトリを作成する
         @param name
         """
-        cmd = "cd ../ && mkdir {0}".format(name)
-        subprocess.call(cmd, shell=True)
-
-        print("creating...")
-        for k in range(len(app_files)):
-            cmd = "cd ../{0} && touch {1}".format(name, app_files[k])
-            print(name + "/" + app_files[k])
-            subprocess.call(cmd, shell=True)
+        print("------ app dir --------")
+        self.__create_files(name, files=app_files)
 
 
 if __name__ == '__main__':
