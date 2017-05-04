f919fe82445c68f6125b3367fcdb12327e516f1f
kubocker <kubocker@gmail.com>
2017-05-04 13:21:58 +0900

    一旦コミット

--- manage.py
+++ manage.py
@@ -1,25 +1,39 @@
 import subprocess
 import fire
 
-files = ['__init__.py', 'forms.py', 'views.py', 'models.py']
+project_files = ['__init__.py', 'settings.py', 'app.py']
+app_files = ['__init__.py', 'forms.py', 'views.py', 'models.py']
 
 
 class Command(object):
 
     def hello(self, username):
+        """ テストテスト """
         return "Hello, " + username + " !!"
 
     def startproject(self, name):
-        pass
-
-    def startapp(self, name):
+        """ プロジェクトディレクトリを作成する
+        @param name
+        """
         cmd = "cd ../ && mkdir {0}".format(name)
         subprocess.call(cmd, shell=True)
 
         print("creating...")
-        for k in range(len(files)):
-            cmd = "cd ../{0} && touch {1}".format(name, files[k])
-            print(name + "/" + files[k])
+        for k in range(len(project_files) - 1):
+            cmd = "cd ../"
+        subprocess.call(cmd, shell=True)
+
+    def startapp(self, name):
+        """ アプリディレクトリを作成する
+        @param name
+        """
+        cmd = "cd ../ && mkdir {0}".format(name)
+        subprocess.call(cmd, shell=True)
+
+        print("creating...")
+        for k in range(len(app_files)):
+            cmd = "cd ../{0} && touch {1}".format(name, app_files[k])
+            print(name + "/" + app_files[k])
             subprocess.call(cmd, shell=True)
 
 
