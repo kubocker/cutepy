610f969ff6d6cb7dc1a821d34e047e9cc0241e6c
kubocker <kubocker@gmail.com>
2017-05-15 20:48:23 +0900

    setup fix

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
-
+from cutepy import __version__ as version
 from setuptools import setup, find_packages
 
+
 setup(name='cutepy',
-      version='0.1.1',
+      version=version,
       description='common ui and commands for pyqt5',
       author='kubocker',
       author_email='kubocker@gmail.com',
@@ -12,11 +13,23 @@ setup(name='cutepy',
       license="MIT",
       packages=find_packages(),
       install_requires=[
+          "django-contrib-comments",
+          "django >= 1.8, < 1.11",
+          "filebrowser_safe >= 0.4.6",
+          "grappelli_safe >= 0.4.5",
+          "tzlocal >= 1.0",
+          "bleach >= 2.0",
+          "beautifulsoup4 >= 4.5.3",
+          "requests >= 2.1.0",
+          "requests-oauthlib >= 0.4",
+          "future >= 0.9.0",
+          "pillow",
+          "chardet",
           "pyqt5",
           "fire",
       ],
       entry_points={
           'console_scripts': [
-              'cute.py = cutepy.manage:create'
+              'cute = cutepy.manage:create'
           ]
       },)
